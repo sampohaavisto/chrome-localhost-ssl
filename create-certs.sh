b8e64ff1b0ac31a09ec4760faf59f923feeec26f
Sampo Haavisto <sampo.haavisto@motleyagency.com>
2017-05-26 14:33:19 +0300

    Instructions on rootca pw

--- create-certs.sh
+++ create-certs.sh
@@ -11,11 +11,16 @@ set -e
 mkdir root-cert/
 
 echo ""
-echo "JUST HIT ENTER BUT SET localhost AS COMMON NAME (FQDN)"
-echo ""
+
+echo "USE SOME SIMPLE PASSWORD FOR ROOTCA, like 1234"
 
 openssl genrsa -des3 -out root-cert/rootCA.key 2048;
 openssl req -x509 -new -nodes -key root-cert/rootCA.key -sha256 -days 1024 -out root-cert/rootCA.pem;
+
+
+echo ""
+echo "JUST HIT ENTER BUT SET localhost AS COMMON NAME (FQDN)"
+echo ""
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout localhost.key -out localhost.crt;
 openssl req -new -sha256 -key localhost.key -out localhost.csr;
